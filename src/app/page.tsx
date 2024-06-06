f0def163715c55338be5c939f7280c99f584b4ee
Ralph Parin <r.parin070@gmail.com>
2024-06-06 13:09:02 -0700

    Home: utilize border component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import Link from "next/link";
 import Image from "next/image";
 import { webLogo } from "@/constants/icons";
-import Footer from "@/components/Footer";
+import Border from "@/components/Border";
 
 export default function Home() {
   const items = [
@@ -37,38 +37,35 @@ export default function Home() {
 
   return (
     <>
-      <div className="h-dvh rounded-[45px] rounded-b-[50px] bg-border p-[25px] pb-[50px]">
-        <div className="h-full w-full overflow-auto rounded-[20px] rounded-b-[10px] border bg-innerBg">
-          <div className="flex h-[60vh] flex-col items-center justify-center gap-3 text-center">
-            <Link href={"/"}>
-              <Image
-                className="h-auto w-72 md:w-96"
-                src={webLogo.img}
-                alt={webLogo.alt}
-              />
-            </Link>
-            <ReactSearchAutocomplete
-              className="w-[75%] md:w-[50%] lg:w-[30%]"
-              items={items}
-              onSelect={handleOnSelect}
-              // formatResult={(item: any) => {
-              //   return GraphSearchResult(item);
-              // }}
-              styling={{
-                border: "2.5px solid #FFD29D",
-                lineColor: "rgba(0,0,0,.35)",
-                boxShadow: "rgba(32, 33, 36, 0.28) 0px 1px 12px 0px",
-                fontFamily: "Belanosima",
-              }}
-              placeholder={SEARCH_PLACEHOLDER}
-              showIcon={false}
-              maxResults={5}
-              autoFocus={true}
+      <Border>
+        <div className="flex h-[60vh] flex-col items-center justify-center gap-3 text-center">
+          <Link href={"/"}>
+            <Image
+              className="h-auto w-72 md:w-96"
+              src={webLogo.img}
+              alt={webLogo.alt}
             />
-          </div>
+          </Link>
+          <ReactSearchAutocomplete
+            className="w-[75%] md:w-[50%] lg:w-[30%]"
+            items={items}
+            onSelect={handleOnSelect}
+            // formatResult={(item: any) => {
+            //   return GraphSearchResult(item);
+            // }}
+            styling={{
+              border: "2.5px solid #FFD29D",
+              lineColor: "rgba(0,0,0,.35)",
+              boxShadow: "rgba(32, 33, 36, 0.28) 0px 1px 12px 0px",
+              fontFamily: "Belanosima",
+            }}
+            placeholder={SEARCH_PLACEHOLDER}
+            showIcon={false}
+            maxResults={5}
+            autoFocus={true}
+          />
         </div>
-        <Footer className="pt-3.5 text-black opacity-70" />
-      </div>
+      </Border>
     </>
   );
 }
