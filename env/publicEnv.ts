7fc45b804a65fad2b52a2d19b22c6240677a2e11
Ralph Parin <r.parin070@gmail.com>
2024-06-05 23:40:26 -0700

    update publicEnv file

--- env/publicEnv.ts
+++ env/publicEnv.ts
@@ -2,10 +2,8 @@ import { z } from "zod";
 
 const publicEnvSchema = z.object({
   NEXT_PUBLIC_CLIENT_URL: z.string().url({ message: "Invalid CLIENT URL" }),
-  NEXT_PUBLIC_SERVER_URL: z.string().url({ message: "Invalid SERVER URL" }),
 });
 
 export const publicParsedEnv = publicEnvSchema.parse({
   NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
-  NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
 });
