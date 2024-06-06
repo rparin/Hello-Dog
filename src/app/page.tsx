694ecfbba7f83597d9852b2f806aaf01d1503e3d
Ralph Parin <r.parin070@gmail.com>
2024-06-05 21:11:44 -0700

    implement basic home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,56 @@
+"use client";
+import { APP_NAME, SEARCH_PLACEHOLDER } from "@/constants";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import Link from "next/link";
+
 export default function Home() {
-  return <main className="min-h-dvh bg-background">Hello World</main>;
+  const items = [
+    {
+      id: 0,
+      name: "Cobol",
+    },
+    {
+      id: 1,
+      name: "JavaScript",
+    },
+    {
+      id: 2,
+      name: "Basic",
+    },
+    {
+      id: 3,
+      name: "PHP",
+    },
+    {
+      id: 4,
+      name: "Java",
+    },
+  ];
+
+  const handleOnSelect = (item: any) => {
+    // the item selected
+    console.log(item);
+  };
+
+  return (
+    <>
+      <div className="flex h-[80vh] flex-col items-center justify-center gap-7 text-center">
+        <Link href={"/"}>
+          <h1 className="text-3xl md:text-5xl">{APP_NAME}</h1>
+        </Link>
+        <ReactSearchAutocomplete
+          className="z-[80] w-[60%] md:w-[40%] lg:w-[30%]"
+          items={items}
+          onSelect={handleOnSelect}
+          // formatResult={(item: any) => {
+          //   return GraphSearchResult(item);
+          // }}
+          placeholder={SEARCH_PLACEHOLDER}
+          showIcon={false}
+          maxResults={5}
+          autoFocus={false}
+        />
+      </div>
+    </>
+  );
 }
---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+const APP_NAME = "Hello Dog";
+
+const SEARCH_PLACEHOLDER = "Enter the name of a dog breed";
+const SHORT_SEARCH_PLACEHOLDER = "Search dog breed";
+
+const CR = "©️ 2024. Developed by Ralph Parin.";
+
+export { APP_NAME, SEARCH_PLACEHOLDER, SHORT_SEARCH_PLACEHOLDER, CR };
