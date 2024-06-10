55f5a5c323078e88484a529371ac37400ec5186c
Ralph Parin <r.parin070@gmail.com>
2024-06-09 19:16:43 -0700

    DogBreed: Refactor layout

--- src/app/[dogBreed]/layout.tsx
+++ src/app/[dogBreed]/layout.tsx
@@ -8,14 +8,5 @@ export default function dogResultsLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <main className="flex min-h-screen flex-col">
-      <div className="flex-1">
-        {/* <Header /> */}
-        {children}
-      </div>
-      {/* Display footer at the bottom */}
-      {/* <Footer /> */}
-    </main>
-  );
+  return { children };
 }
