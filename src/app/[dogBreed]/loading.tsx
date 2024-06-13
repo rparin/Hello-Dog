cbf669738a648b44f8f183f31616df067032e84b
Ralph Parin <r.parin070@gmail.com>
2024-06-12 20:23:24 -0700

    Util: Create & Use getRandItem function

--- src/app/[dogBreed]/loading.tsx
+++ src/app/[dogBreed]/loading.tsx
@@ -1,16 +1,15 @@
 import Border from "@/components/Border";
 import { LoadingText } from "@data/LoadingText";
+import { getRandItem } from "@/lib/utils";
 
 export default function loading() {
-  var item = LoadingText[Math.floor(Math.random() * LoadingText.length)];
-
   return (
     <main>
       <Border
         styling={{ innerClassName: "h-full w-full content-center text-center" }}
       >
         <h1 className="text-2xl text-black/80 md:text-3xl lg:text-4xl xl:text-5xl">
-          {item}
+          {getRandItem(LoadingText)}
         </h1>
       </Border>
     </main>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getRandItem(arr: Array<any>) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
