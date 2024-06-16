a9b99cc8a0e167298392aca59aa31a9056699d4d
Ralph Parin <r.parin070@gmail.com>
2024-06-15 21:56:14 -0700

    Actions: Throw errors for invalid fetch or bad response

--- src/lib/actions/DogFact.ts
+++ src/lib/actions/DogFact.ts
@@ -2,17 +2,32 @@
 import { DogFactSchema } from "../schema/DogFact";
 
 export async function getDogFact() {
-  const res = await fetch("https://dogapi.dog/api/v2/facts", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/vnd.api+json; charset=utf-8",
-    },
-    cache: "no-store",
-  });
+  var res;
+
+  try {
+    res = await fetch("https://dogapi.dog/api/v2/facts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/vnd.api+json; charset=utf-8",
+      },
+      cache: "no-store",
+    });
+  } catch (error) {
+    throw new Error("Dog Fact: Fetch Error", { cause: error });
+  }
+
+  if (!res?.ok) {
+    throw new Error("Dog Fact: Response Error", {
+      cause: `HTTP Response Code: ${res?.status}`,
+    });
+  }
+
   const data = await res.json();
   const result = DogFactSchema.safeParse(data);
   if (!result.success) {
-    throw new Error("Dog Fact: Invalid API Response format");
+    throw new Error("Dog Fact: Invalid API Response format", {
+      cause: result.error,
+    });
   } else {
     return result.data.data[0].attributes.body;
   }
--- src/lib/actions/DogImageCeo.ts
+++ src/lib/actions/DogImageCeo.ts
@@ -2,20 +2,32 @@
 import { DogImageCeoSchema } from "../schema/DogImageCeo";
 
 export async function getCeoDogImage(breedUrl: string) {
-  const res = await fetch(
-    `https://dog.ceo/api/breed/${breedUrl}/images/random`,
-    {
+  var res;
+
+  try {
+    res = await fetch(`https://dog.ceo/api/breed/${breedUrl}/images/random`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
       cache: "no-store",
-    },
-  );
+    });
+  } catch (error) {
+    throw new Error("Dog Ceo Image: Fetch Error", { cause: error });
+  }
+
+  if (!res?.ok) {
+    throw new Error("Dog Ceo Image: Response Error", {
+      cause: `HTTP Response Code: ${res?.status}`,
+    });
+  }
+
   const data = await res.json();
   const result = DogImageCeoSchema.safeParse(data);
   if (!result.success) {
-    throw new Error("Dog Ceo Image: Invalid API Response format");
+    throw new Error("Dog Ceo Image: Invalid API Response format", {
+      cause: result.error,
+    });
   } else {
     return await result.data;
   }
