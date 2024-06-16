0cfa0b69b3e014b19001a4e332ec0eca1a96882f
Ralph Parin <r.parin070@gmail.com>
2024-06-15 22:36:47 -0700

    WebInfo: Update description

--- src/constants/webInfo.ts
+++ src/constants/webInfo.ts
@@ -6,7 +6,7 @@ const SHORT_SEARCH_PLACEHOLDER = "Search dog breed";
 const CR = "©️ 2024. Designed and Developed by Ralph Parin.";
 
 const DESCRIPTION =
-  "A website that brings you interesting dog facts, stunning dog images, and detailed dog information. Dog facts are sourced from Dog API, photos are from Unsplash or Dog.ceo, and dog breed details are retrieved from API Ninjas.";
+  "A website that brings you interesting dog facts, stunning dog images, and detailed dog information. Dog facts are sourced from Dog API, photos are from Dog.ceo, and dog breed details are retrieved from API Ninjas.";
 
 export {
   APP_NAME,
