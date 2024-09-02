ef5c9c6cf1b49a9bc9fd6f2fe6cf9c8cfd12c994
Ralph Parin <r.parin070@gmail.com>
2024-09-02 04:52:26 -0700

    Create Dog DTO classes

---
+++ Server/src/main/java/com/HelloDog/Backend/dto/DogFactDto.java
@@ -0,0 +1,10 @@
+package com.HelloDog.Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DogFactDto {
+    private String dogFact;
+}
---
+++ Server/src/main/java/com/HelloDog/Backend/dto/DogInfoDto.java
@@ -0,0 +1,34 @@
+package com.HelloDog.Backend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DogInfoDto {
+    private String name;
+    private String img;
+    private int goodWithChildren;
+    private int goodWithOtherDogs;
+    private int goodWithStrangers;
+    private int shedding;
+    private int grooming;
+    private int drooling;
+    private int coatLength;
+    private int playfulness;
+    private int protectiveness;
+    private int trainability;
+    private int energy;
+    private int barking;
+    private int minLifeExpectancy;
+    private int maxLifeExpectancy;
+    private int maxHeightMale;
+    private int minHeightMale;
+    private int maxHeightFemale;
+    private int minHeightFemale;
+    private int maxWeightMale;
+    private int minWeightMale;
+    private int maxWeightFemale;
+    private int minWeightFemale;
+}
