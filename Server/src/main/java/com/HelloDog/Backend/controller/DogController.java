5beae9be4f67ba837772505388f9c1ba5383d3fe
Ralph Parin <r.parin070@gmail.com>
2024-09-03 00:34:12 -0700

    Server: Implement DogInfo get request

--- Server/src/main/java/com/HelloDog/Backend/controller/DogController.java
+++ Server/src/main/java/com/HelloDog/Backend/controller/DogController.java
@@ -2,8 +2,10 @@ package com.HelloDog.Backend.controller;
 
 import com.HelloDog.Backend.dto.DogFactDto;
 import com.HelloDog.Backend.dto.DogImagesDto;
+import com.HelloDog.Backend.dto.DogInfoDto;
 import com.HelloDog.Backend.service.DogFactService;
 import com.HelloDog.Backend.service.DogImageService;
+import com.HelloDog.Backend.service.DogInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.*;
 public class DogController {
     private final DogFactService dogFactService;
     private final DogImageService dogImageService;
+    private final DogInfoService dogInfoService;
 
     @Autowired
-    public DogController(DogFactService dogFactService, DogImageService dogImageService) {
+    public DogController(DogFactService dogFactService, DogImageService dogImageService, DogInfoService dogInfoService) {
         this.dogFactService = dogFactService;
         this.dogImageService = dogImageService;
+        this.dogInfoService = dogInfoService;
     }
 
     @GetMapping("/dogfact")
@@ -30,5 +34,10 @@ public class DogController {
     @GetMapping("/dogImage/{breed}")
     public ResponseEntity<DogImagesDto> getDogImgs(@PathVariable String breed) {
         return dogImageService.getDogImgs(breed);
+    }
+
+    @GetMapping("/dogInfo/{breed}")
+    public ResponseEntity<DogInfoDto> getDogInfo(@PathVariable String breed) {
+        return dogInfoService.getDogInfo(breed);
     }
 }
