61e26eb4a217b904755cc472f349f875ea54ce5c
Ralph Parin <r.parin070@gmail.com>
2024-09-02 22:16:43 -0700

    Server: Update DogFactDto & DogImagesDto, add AllArgsConstructor

--- Server/src/main/java/com/HelloDog/Backend/dto/DogImagesDto.java
+++ Server/src/main/java/com/HelloDog/Backend/dto/DogImagesDto.java
@@ -1,10 +1,12 @@
 package com.HelloDog.Backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class DogImagesDto {
     private List<String> imgs;
     private int len;
