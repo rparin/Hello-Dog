efba976bdeefef559e9f7463dbfd277fe63c2228
Ralph Parin <r.parin070@gmail.com>
2024-09-02 21:55:19 -0700

    Server: Update Dtos remove @AllArgsConstructor

--- Server/src/main/java/com/HelloDog/Backend/dto/DogImagesDto.java
+++ Server/src/main/java/com/HelloDog/Backend/dto/DogImagesDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 public class DogImagesDto {
     private List<String> imgs;
     private int len;
--- Server/src/main/java/com/HelloDog/Backend/dto/DogInfoDto.java
+++ Server/src/main/java/com/HelloDog/Backend/dto/DogInfoDto.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class DogInfoDto {
     private String name;
     private String img;
