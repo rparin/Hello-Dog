52ecb11dd02ccef8775e4b01ff6b4fbf910ce8bf
Ralph Parin <r.parin070@gmail.com>
2024-09-12 22:06:25 -0700

    Server: Implement logging

--- Server/src/main/java/com/HelloDog/Backend/controller/DogController.java
+++ Server/src/main/java/com/HelloDog/Backend/controller/DogController.java
@@ -3,12 +3,21 @@ package com.HelloDog.Backend.controller;
 import com.HelloDog.Backend.dto.DogFactDto;
 import com.HelloDog.Backend.dto.DogImagesDto;
 import com.HelloDog.Backend.dto.DogInfoDto;
+
 import com.HelloDog.Backend.service.DogFactService;
 import com.HelloDog.Backend.service.DogImageService;
 import com.HelloDog.Backend.service.DogInfoService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping("/api")
@@ -16,6 +25,7 @@ public class DogController {
     private final DogFactService dogFactService;
     private final DogImageService dogImageService;
     private final DogInfoService dogInfoService;
+    private static final Logger log = LoggerFactory.getLogger(DogController.class);
 
     @Autowired
     public DogController(DogFactService dogFactService, DogImageService dogImageService, DogInfoService dogInfoService) {
@@ -26,16 +36,19 @@ public class DogController {
 
     @GetMapping("/dogfact")
     public ResponseEntity<DogFactDto> getDogFact(@RequestParam(value = "max_length", defaultValue = "200", required = false) int len) {
+        log.info("Getting Dog Fact");
         return dogFactService.getDogFact(len);
     }
 
     @GetMapping("/dogImage/{breed}")
     public ResponseEntity<DogImagesDto> getDogImgs(@PathVariable String breed) {
+        log.info("Getting Dog Images");
         return dogImageService.getDogImgs(breed);
     }
 
     @GetMapping("/dogInfo/{breed}")
     public ResponseEntity<DogInfoDto> getDogInfo(@PathVariable String breed) {
+        log.info("Getting Dog Info");
         return dogInfoService.getDogInfo(breed);
     }
 }
--- Server/src/main/java/com/HelloDog/Backend/exceptions/DogAPIHandler.java
+++ Server/src/main/java/com/HelloDog/Backend/exceptions/DogAPIHandler.java
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @ControllerAdvice(assignableTypes = DogController.class)
 public class DogAPIHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(DogAPIHandler.class);
 
     @ExceptionHandler(DogAPIException.class)
     private ResponseEntity<ErrorObject> handleDogAPIException(DogAPIException ex, WebRequest request) {
@@ -18,6 +22,7 @@ public class DogAPIHandler {
         errorObject.setStatusCode(ex.getHttpStatus().value());
         errorObject.setMessage(String.format("%s", ex.getMessage()));
         errorObject.setTimestamp(new Date());
+        log.error("Caught Error in handleDogAPIException", ex);
         return new ResponseEntity<>(errorObject, ex.getHttpStatus());
     }
 
