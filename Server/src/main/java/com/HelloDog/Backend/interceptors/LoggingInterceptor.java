a03ac5f9d8437c3068e2dd0751fc12ebb228cd38
Ralph Parin <r.parin070@gmail.com>
2024-09-14 13:07:21 -0700

    Server: Add query info to LoggingInterceptor

--- Server/src/main/java/com/HelloDog/Backend/interceptors/LoggingInterceptor.java
+++ Server/src/main/java/com/HelloDog/Backend/interceptors/LoggingInterceptor.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 @Component  // Registers this class as a Spring component
 public class LoggingInterceptor implements HandlerInterceptor {
@@ -17,10 +16,9 @@ public class LoggingInterceptor implements HandlerInterceptor {
     @Override
     // Logs HTTP method and URI before the request is handled
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        LOGGER.info("Request: {} {}", request.getMethod(), request.getRequestURI());
+        LOGGER.info("Request: {} {} Query:{}", request.getMethod(), request.getRequestURI(), request.getQueryString());
         return true;  // Allows the request to proceed
     }
-
 
     @Override
     // Logs response status and URI after request completion. Logs exceptions if any
