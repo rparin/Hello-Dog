a2b9425d7078005abe371d9e07a32c114e6b7937
Ralph Parin <r.parin070@gmail.com>
2024-09-17 11:22:10 -0700

    Server: DogFactDto & DogImagesDto, add builder

--- Server/src/main/java/com/HelloDog/Backend/dto/DogImagesDto.java
+++ Server/src/main/java/com/HelloDog/Backend/dto/DogImagesDto.java
@@ -1,11 +1,13 @@
 package com.HelloDog.Backend.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class DogImagesDto {
     private List<String> imgs;
