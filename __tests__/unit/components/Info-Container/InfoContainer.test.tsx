8c63801baac8338c3e89d45fa2333907dcc8464e
Ralph Parin <r.parin070@gmail.com>
2024-10-25 09:28:21 -0700

    Tests: Write Info Container unit tests

--- __tests__/unit/components/Info-Container/InfoContainer.test.tsx
+++ __tests__/unit/components/Info-Container/InfoContainer.test.tsx
@@ -1,38 +1,38 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
 import InfoContainer from "@/components/Info-Container/InfoContainer";
 
-expect.extend(toHaveNoViolations);
-
-test("should have no accessibility violations", async () => {
-  const { container } = render(
-    <InfoContainer
-      title="Physical Stats"
-      info={[
-        {
-          name: "Life Expectancy",
-          value: {
-            other: "12 - 15 years",
-          },
-        },
-      ]}></InfoContainer>
-  );
-  const results = await axe(container);
-  expect(results).toHaveNoViolations();
-});
+const PROPS = {
+  title: "Physical Stats",
+  info1: {
+    name: "Life Expectancy",
+    value: {
+      other: "12 - 15 years",
+    },
+  },
+};
 
 test("it renders component unchanged", () => {
   const { container } = render(
-    <InfoContainer
-      title="Physical Stats"
-      info={[
-        {
-          name: "Life Expectancy",
-          value: {
-            other: "12 - 15 years",
-          },
-        },
-      ]}></InfoContainer>
+    <InfoContainer title={PROPS.title} info={[PROPS.info1]}></InfoContainer>
   );
   expect(container).toMatchSnapshot();
+});
+
+test("it should show Info container title", () => {
+  render(
+    <InfoContainer title={PROPS.title} info={[PROPS.info1]}></InfoContainer>
+  );
+  const infoTitleElement = screen.getByText(PROPS.title);
+  expect(infoTitleElement).toBeInTheDocument();
+});
+
+expect.extend(toHaveNoViolations);
+test("should have no accessibility violations", async () => {
+  const { container } = render(
+    <InfoContainer title={PROPS.title} info={[PROPS.info1]}></InfoContainer>
+  );
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
 });
--- __tests__/unit/components/Info-Container/Infobar.test.tsx
+++ __tests__/unit/components/Info-Container/Infobar.test.tsx
@@ -1,32 +1,29 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
 import Infobar from "@/components/Info-Container/Infobar";
 
-expect.extend(toHaveNoViolations);
-
-test("should have no accessibility violations", async () => {
-  const { container } = render(
-    <Infobar
-      barValue={{
-        name: "Life Expectancy",
-        value: {
-          other: "12 - 15 years",
-        },
-      }}></Infobar>
-  );
-  const results = await axe(container);
-  expect(results).toHaveNoViolations();
-});
+const PROPS = {
+  name: "Life Expectancy",
+  value: {
+    other: "12 - 15 years",
+  },
+};
 
 test("it renders component unchanged", () => {
-  const { container } = render(
-    <Infobar
-      barValue={{
-        name: "Life Expectancy",
-        value: {
-          other: "12 - 15 years",
-        },
-      }}></Infobar>
-  );
+  const { container } = render(<Infobar barValue={PROPS}></Infobar>);
   expect(container).toMatchSnapshot();
+});
+
+test("it shows Infobar name", () => {
+  render(<Infobar barValue={PROPS}></Infobar>);
+  const infobarNameElement = screen.getByText(PROPS.name);
+  expect(infobarNameElement).toBeInTheDocument();
+});
+
+expect.extend(toHaveNoViolations);
+test("should have no accessibility violations", async () => {
+  const { container } = render(<Infobar barValue={PROPS}></Infobar>);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
 });
