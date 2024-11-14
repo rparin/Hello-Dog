aa3d00275a0ba9c96fc52ef77717cfe57e31c571
Ralph Parin <r.parin070@gmail.com>
2024-11-14 15:53:04 -0800

    Test: Search, add comments

--- __tests__/e2e/Search.test.tsx
+++ __tests__/e2e/Search.test.tsx
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { PAGE } from "@test-utils/playwright.utils";
 import { APP_NAME, SEARCH_PLACEHOLDER } from "@/constants/webInfo";
 
+// Slow down test by ms
 // test.use({
 //   launchOptions: {
 //     slowMo: 1_000,
@@ -22,8 +23,8 @@ test.describe("Search for both types of corgi", () => {
     await searchBar.fill("corgi");
     await page.keyboard.press("Enter");
 
+    //Get the search element and check if its in view
     const corgiSearchBar = page.getByPlaceholder("corgi");
-
     await expect(corgiSearchBar).toBeInViewport();
     await expect(corgiSearchBar).toBeEmpty();
   });
@@ -34,13 +35,14 @@ test.describe("Search for both types of corgi", () => {
     const searchBar = page.getByPlaceholder(SEARCH_PLACEHOLDER);
     await expect(searchBar).toBeVisible();
 
+    //Select the 2nd option from the search bar drop down menu
     await searchBar.fill("corgi");
     await page.keyboard.press("ArrowDown");
     await page.keyboard.press("ArrowDown");
     await page.keyboard.press("Enter");
 
+    //Get the search element and check if its in view
     const corgiSearchBar = page.getByPlaceholder("pembroke");
-
     await expect(corgiSearchBar).toBeInViewport();
     await expect(corgiSearchBar).toBeEmpty();
   });
@@ -63,7 +65,7 @@ test("it should search for corgi, shiba, then go back to home page", async ({
   await expect(corgiSearchBar).not.toBeFocused();
   await expect(page).toHaveTitle("Hello Dog - Cardigan Welsh Corgi");
 
-  //Corgi Search
+  //Shiba Search
   await corgiSearchBar.fill("shiba");
   await page.keyboard.press("Enter");
   const shibaSearchBar = page.getByPlaceholder("shiba");
