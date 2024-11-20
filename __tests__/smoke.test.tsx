1b345c9afe73d9074e7e6295d5073ce3557deaa9
Ralph Parin <r.parin070@gmail.com>
2024-11-19 16:06:29 -0800

    Package: Update lint-stage smoke test cmd

--- __tests__/smoke.test.tsx
+++ __tests__/smoke.test.tsx
@@ -1,5 +1,4 @@
 import App from "@/app/page";
-// import DogBreed from "@/app/[dogBreed]/page";
 import { act, render } from "@testing-library/react";
 import { axe, toHaveNoViolations } from "jest-axe";
 
@@ -11,16 +10,4 @@ describe("testing App Accessibility", () => {
     const results = await act(async () => axe(container));
     expect(results).toHaveNoViolations();
   });
-
-  // test("it (DogBreed Page) should have no accessibility violations", async () => {
-  //   const { container } = render(
-  //     <DogBreed
-  //       params={{
-  //         dogBreed: "Cardigan_Welsh_Corgi",
-  //       }}
-  //     />
-  //   );
-  //   const results = await act(async () => axe(container));
-  //   expect(results).toHaveNoViolations();
-  // });
 });
